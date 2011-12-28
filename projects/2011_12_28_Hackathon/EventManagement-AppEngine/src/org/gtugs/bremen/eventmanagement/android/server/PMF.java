6caa25d7722111b29d52ff7d12a4d45d4c14a44d
b.ahlfeld@gmail.com <b.ahlfeld@gmail.com@bc405e18-e45b-e554-df79-a0bdb10d3be3>
2011-12-28 13:35:48 +0000

    pmf

---
+++ projects/2011_12_28_Hackathon/EventManagement-AppEngine/src/org/gtugs/bremen/eventmanagement/android/server/PMF.java
@@ -0,0 +1,15 @@
+package org.gtugs.bremen.eventmanagement.android.server;
+
+import javax.jdo.JDOHelper;
+import javax.jdo.PersistenceManagerFactory;
+
+public class PMF {
+
+	private static final PersistenceManagerFactory pmf = JDOHelper.getPersistenceManagerFactory("transactions-optional");
+	
+	private PMF() {}
+
+    public static PersistenceManagerFactory get() {
+        return pmf;
+    }
+}
