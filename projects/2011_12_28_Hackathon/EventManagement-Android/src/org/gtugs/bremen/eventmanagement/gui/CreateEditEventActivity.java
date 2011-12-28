914a3298a5a042d28d68375909a7aa490ab09ed5
sliedtke57@gmail.com <sliedtke57@gmail.com@bc405e18-e45b-e554-df79-a0bdb10d3be3>
2011-12-28 11:43:11 +0000

    added CreateEditEventActivity.java

---
+++ projects/2011_12_28_Hackathon/EventManagement-Android/src/org/gtugs/bremen/eventmanagement/gui/CreateEditEventActivity.java
@@ -0,0 +1,18 @@
+package org.gtugs.bremen.eventmanagement.gui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CreateEditEventActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		// TODO if information in savedInstanceState exists it is an edit
+		
+		// TODO show create/edit form
+		
+		// TODO implement onclick for saving
+	}
+}
