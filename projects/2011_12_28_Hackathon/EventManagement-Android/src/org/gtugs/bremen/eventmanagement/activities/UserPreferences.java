020845df4f7bc0e6c78d08cab8379c26f4713078
sliedtke57@gmail.com <sliedtke57@gmail.com@bc405e18-e45b-e554-df79-a0bdb10d3be3>
2011-12-28 11:37:49 +0000

    added UserPreferences.java

---
+++ projects/2011_12_28_Hackathon/EventManagement-Android/src/org/gtugs/bremen/eventmanagement/activities/UserPreferences.java
@@ -0,0 +1,13 @@
+package org.gtugs.bremen.eventmanagement.activities;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class UserPreferences extends PreferenceActivity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+//		addPreferencesFromResource(R.xml.userpreferences);
+	}
+}
