8129dee05615cb3358ca2821e60237c90b047a27
philipschnake@googlemail.com <philipschnake@googlemail.com@bc405e18-e45b-e554-df79-a0bdb10d3be3>
2011-12-28 16:38:16 +0000

    getallevents works

--- projects/2011_12_28_Hackathon/EventManagement-AppEngine/src/org/gtugs/bremen/eventmanagement/android/servlets/GetAllEvents.java
+++ projects/2011_12_28_Hackathon/EventManagement-AppEngine/src/org/gtugs/bremen/eventmanagement/android/servlets/GetAllEvents.java
@@ -1,40 +1,67 @@
 package org.gtugs.bremen.eventmanagement.android.servlets;
 
-import java.util.Date;
 import java.util.List;
 
 import javax.servlet.http.HttpServlet;
 
 import org.gtugs.bremen.eventmanagement.android.persistence.PersistEvent;
-import org.gtugs.bremen.eventmanagement.android.server.Attendee;
 import org.gtugs.bremen.eventmanagement.android.server.Event;
-import org.gtugs.bremen.eventmanagement.android.server.Talk;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+
+
+
 
 @SuppressWarnings("serial")
 public class GetAllEvents extends HttpServlet{
 
-	protected void doPost(javax.servlet.http.HttpServletRequest req,
-			javax.servlet.http.HttpServletResponse resp)
-		throws javax.servlet.ServletException, java.io.IOException {
+	protected void doGet(javax.servlet.http.HttpServletRequest req,	javax.servlet.http.HttpServletResponse resp) throws javax.servlet.ServletException, java.io.IOException {
+				
+		JSONObject ev = new JSONObject();
+		JSONArray jsonArray = new JSONArray();
+		
+		PersistEvent persistEvent = new PersistEvent();
 		
 		
-		PersistEvent persistEvent = PersistEvent.
-		Event event = new Event();
+		List<Event> events = persistEvent.getAllEvents();
 		
 		
-	
-		String name = event.getName();
-		String location = event.getLocation();
-		Date startDate = event.getStartDate();
-		Date endDate = event.getEndDate();
-		String url = event.getUrl();
-		List<Attendee> attendies = event.getAttendies();
-		Attendee admin;
-		List<Talk> talks = event.getTalks();
-		int maxAttendees = event.getMaxAttendees();
+		for (Event event : events){
+			String name = event.getName();
+			Key id = event.getPkId();
+			String keys = KeyFactory.keyToString(id);
+
+			JSONObject obj = new JSONObject();
+			try {
+				obj.put("name", name);
+			} catch (JSONException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			try {
+				obj.put("key", keys);
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+				
+			jsonArray.put(obj);
+			
+		}
 		
-		
-		
+		try {
+			ev.put("events", jsonArray);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+				
+		resp.setContentType("text/plain");
+		resp.getWriter().append(ev.toString());
 		
 	}
 
