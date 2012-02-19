c3a00881bd5b83f680396bdce52fb2be35060566
xmarcus89@googlemail.com <xmarcus89@googlemail.com@bc405e18-e45b-e554-df79-a0bdb10d3be3>
2012-02-19 17:52:10 +0000

    Portal collision points - final version so far, but NOT working at all.

--- projects/2012_02_18_GADC/MultiPlayerLabyrinth/src/org/gtugs/bremen/multilabyrinth/scene/impl/handler/PortalUpdateHandler.java
+++ projects/2012_02_18_GADC/MultiPlayerLabyrinth/src/org/gtugs/bremen/multilabyrinth/scene/impl/handler/PortalUpdateHandler.java
@@ -22,16 +22,39 @@ public class PortalUpdateHandler implements IUpdateHandler{
 		this.networkCommunication = networkCommunication;
 	}
 	
+	// Completely not working yet !! 
 	@Override
 	public void onUpdate(final float pSecondsElapsed) {
 		if(ball.collidesWith(portal)) {
 			portal.setColor(1, 0, 0);
 			
-//			Log.d("PortalUpdaterHandler", "ball getSkewCenterX " + ball.getSceneCenterCoordinates()[0]);
-			Log.d("PortalUpdaterHandler", "ball getSkewCenterY " + ball.getSceneCenterCoordinates()[1]);
+			Log.d("PortalUpdaterHandler", "ball getSceneCenterCoordinatesX " + ball.getSceneCenterCoordinates()[0]);
+			Log.d("PortalUpdaterHandler", "ball getSceneCenterCoordinatesY " + ball.getSceneCenterCoordinates()[1]);
 			
-//			int collisionPosY
-//			portal.
+			float portalX1 = portal.getX1();
+			float portalY1 = portal.getY1();
+			float portalX2 = portal.getX2();
+			float portalY2 = portal.getY2();
+//			
+			float collisionPosX = ball.getSceneCenterCoordinates()[0];
+			float collisionPosY = ball.getSceneCenterCoordinates()[1];
+			collisionPosY = collisionPosY < portalY1 ? portalY1 : (collisionPosY < portalY2 ? collisionPosY : portalY2);
+			
+			// entry ration from Y1 (so Y1 = 0%, Y2 = 100%)
+			float rawEntry = 1 - ((portalY2 - collisionPosY) / (portalY2-portalY1)) + 0.3f;
+
+			Log.d("PortalUpdaterHandler", "RAW: collisionY = " + collisionPosY);
+			Log.d("PortalUpdaterHandler", "RAW: portalY2 = " + portalY2);
+			Log.d("PortalUpdaterHandler", "RAW: portalY1 = " + portalY1);
+			Log.d("PortalUpdaterHandler", "RAW: rawEntry = 1 - CY / (PY2-PY1) + 0.3 = " + rawEntry);
+			float entry = rawEntry < 0 ? 0 : (rawEntry < 1 ? rawEntry : 1);
+			Log.d("PortalUpdaterHandler", "entry = " + entry);
+			
+			// Also necessary: direction (angle), velocity
+			if(this.networkCommunication != null) {
+				this.networkCommunication.sendPortalCollision("12345", 50, entry, 30);
+			}
+			
 		} else {
 			portal.setColor(0, 1, 0);
 		}
