08ab87c0c8a9f7fb83d5de775f15e495e6e6b672
mtthsfrdrch <mtthsfrdrch@bc405e18-e45b-e554-df79-a0bdb10d3be3>
2012-02-19 11:42:52 +0000

    startgame dialog

--- projects/2012_02_18_GADC/MultiPlayerLabyrinth/src/org/gtugs/bremen/multilabyrinth/gui/dialog/StartGameDialog.java
+++ projects/2012_02_18_GADC/MultiPlayerLabyrinth/src/org/gtugs/bremen/multilabyrinth/gui/dialog/StartGameDialog.java
@@ -13,15 +13,21 @@ import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 /**
  * @author peacei
  *
  */
 public class StartGameDialog extends DialogFragment {
+	
+	private Spinner levelSpinner, typeSpinner;
 	
 	public static StartGameDialog newInstance() {
 		return new StartGameDialog();
@@ -32,34 +38,64 @@ public class StartGameDialog extends DialogFragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState) {
 		ViewGroup viewGroup = (ViewGroup) inflater.inflate(R.layout.startgame_dialog, container, false);
 		
-		this.getDialog().setTitle(R.string.choose_type_text);
+		this.getDialog().setTitle(R.string.start_game_title);
 		
-		Button but = (Button) viewGroup.findViewById(R.id.singleBut);
-		but.setOnClickListener(new OnClickListener() {
-			
+		levelSpinner = (Spinner) viewGroup.findViewById(R.id.level_spinner);
+		typeSpinner = (Spinner) viewGroup.findViewById(R.id.type_spinner);
+	    
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+	            this.getActivity().getApplicationContext(), R.array.types, android.R.layout.simple_spinner_item);
+	    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+	    typeSpinner.setAdapter(adapter);
+	    
+	    
+	    
+	    typeSpinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long i) {
+            	switch(position) {
+            		case 0:	
+            			ArrayAdapter<CharSequence> singleAdapter = ArrayAdapter.createFromResource(
+            		            StartGameDialog.this.getActivity().getApplicationContext(), R.array.single_levels, android.R.layout.simple_spinner_item);
+            		    singleAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            		    levelSpinner.setAdapter(singleAdapter);
+            			break;
+            		case 1: case 2: default:	
+            			ArrayAdapter<CharSequence> multiAdapter = ArrayAdapter.createFromResource(
+            		            StartGameDialog.this.getActivity().getApplicationContext(), R.array.multi_levels, android.R.layout.simple_spinner_item);
+            		    multiAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            		    levelSpinner.setAdapter(multiAdapter);
+            	}
+            }
+            public void onNothingSelected(AdapterView<?> parent) { }
+	    });
+	    
+	    Button button = (Button) viewGroup.findViewById(R.id.startBut);
+	    button.setOnClickListener(new OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
-				startGame(GameMode.SINGLE);	
+				int position = typeSpinner.getSelectedItemPosition();
+				
+				switch(position) {
+					case 0: startGame(GameMode.SINGLE); break;
+					case 1: startGame(GameMode.MULTI_CHALLENGE); break;
+					case 2: startGame(GameMode.MULTI_COOP); break;
+					default:
+				}
 			}
 		});
-		
-		but = (Button) viewGroup.findViewById(R.id.challengeBut);
-		but.setOnClickListener(new OnClickListener() {
-			
+	    
+	    button = (Button) viewGroup.findViewById(R.id.cancelBut);
+	    button.setOnClickListener(new OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
-				startGame(GameMode.MULTI_CHALLENGE);	
+				dismiss();
 			}
 		});
-		
-		but = (Button) viewGroup.findViewById(R.id.coopBut);
-		but.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				startGame(GameMode.MULTI_COOP);	
-			}
-		});
+	    
+	    
+	    
 		
 		return viewGroup; 
 	}
