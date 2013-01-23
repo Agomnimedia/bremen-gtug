2dccd1177d772bca7bcd3209153aed415d1ff3ae
sliedtke57@gmail.com <sliedtke57@gmail.com@bc405e18-e45b-e554-df79-a0bdb10d3be3>
2013-01-23 23:32:07 +0000

    added individual marker and fixed bug by rotating the phone

--- projects/2013_02_04_google_maps_api_battle/android/src/de/gdgbremen/mapsapibattle/android/MainActivity.java
+++ projects/2013_02_04_google_maps_api_battle/android/src/de/gdgbremen/mapsapibattle/android/MainActivity.java
@@ -1,15 +1,23 @@
 package de.gdgbremen.mapsapibattle.android;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
+import android.view.View;
 
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMapOptions;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.GoogleMap.InfoWindowAdapter;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import de.gdgbremen.mapsapibattle.library.AbstractMainActivity;
 
@@ -18,6 +26,8 @@ public class MainActivity extends AbstractMainActivity {
 	private SupportMapFragment fragment;
 
 	private GoogleMap map;
+	
+	private List<Marker> markerList;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,17 +36,30 @@ public class MainActivity extends AbstractMainActivity {
 		GooglePlayServicesUtil
 				.isGooglePlayServicesAvailable(getApplicationContext());
 
-		if (savedInstanceState == null) {
-			final FragmentTransaction transaction = getSupportFragmentManager()
-					.beginTransaction();
-			final GoogleMapOptions options = new GoogleMapOptions();
+		final FragmentTransaction transaction = getSupportFragmentManager()
+				.beginTransaction();
+		final GoogleMapOptions options = new GoogleMapOptions();
 
-			// TODO das in setting oder so angeben lassen?
-			// options.zoomControlsEnabled(true);
-			// options.zoomGesturesEnabled(true);
+		options.mapType(GoogleMap.MAP_TYPE_HYBRID);
+		
+		// TODO das in setting oder so angeben lassen?
+		// options.zoomControlsEnabled(true);
+		// options.zoomGesturesEnabled(true);
+		// options.compassEnabled(true);
+		
+		// TODO uisettings ausprobieren
+		
+		
+		if(getSupportFragmentManager().findFragmentByTag("mapFragment") == null){
 			fragment = SupportMapFragment.newInstance(options);
-			transaction.add(R.id.singlefragment, fragment);
-			transaction.commit();
+			transaction.add(R.id.singlefragment, fragment, "mapFragment");
+		}else{
+			fragment = (SupportMapFragment) getSupportFragmentManager().findFragmentByTag("mapFragment");
+		}
+		transaction.commit();
+		
+		if (savedInstanceState == null) {
+		
 		}
 	}
 
@@ -48,12 +71,12 @@ public class MainActivity extends AbstractMainActivity {
 	}
 
 	private void showTest() {
-		float zoom = 18.0f;
+		float zoom = 17.0f;
 		if (zoom > map.getMaxZoomLevel()) {
 			zoom = map.getMaxZoomLevel();
 		}
 		// TODO auswählbar in eigenen dialog o.ä
-		map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+		// map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
 		// map.setMapType(GoogleMap.MAP_TYPE_NONE);
 		// map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 		// map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
@@ -62,6 +85,7 @@ public class MainActivity extends AbstractMainActivity {
 		final CameraPosition position = new CameraPosition.Builder().zoom(zoom)
 				.target(new LatLng(53.075858, 8.80772)).build();
 		map.animateCamera(CameraUpdateFactory.newCameraPosition(position));
+		map.setInfoWindowAdapter(null);
 	}
 
 	@Override
@@ -73,13 +97,45 @@ public class MainActivity extends AbstractMainActivity {
 	@Override
 	protected void showMarker() {
 		// TODO Auto-generated method stub
-
+		markerList = new ArrayList<Marker>();
+		
+		final Marker domMarker = 
+				map.addMarker(
+						new MarkerOptions()
+			        		.position(new LatLng(53.075439,8.808761))
+			        		.title("Das ist unser wunderschöner Dom :-)")
+			        		.snippet("Übrigens wunderschön bei Nacht"));
+		
+		markerList.add(domMarker);
+		
+		final Marker rolandMarker =
+				map.addMarker(
+						new MarkerOptions()
+							.position(new LatLng(53.075853,8.807237))
+							.title("Roland")
+							.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE)));
+		
+		markerList.add(rolandMarker);
+		
+		final Marker rathausMarker =
+				map.addMarker(new MarkerOptions()
+				.position(new LatLng(53.07596,8.807556))
+				.icon(BitmapDescriptorFactory.fromResource(R.drawable.bremer_wappen_smaller)));
+		
+		markerList.add(rathausMarker);
+		
+		map.setInfoWindowAdapter(new TestInfoWindowAdapter());
 	}
 
 	@Override
 	protected void hideMarker() {
 		// TODO Auto-generated method stub
-
+		for(final Marker marker : markerList){
+			marker.remove();
+		}
+		markerList.clear();
+		
+		map.setInfoWindowAdapter(null);
 	}
 
 	@Override
@@ -117,4 +173,35 @@ public class MainActivity extends AbstractMainActivity {
 		// TODO Auto-generated method stub
 
 	}
+	
+	private class TestInfoWindowAdapter implements InfoWindowAdapter{
+
+		private final View mContent;
+		
+		private final View mWindow;
+		
+		public TestInfoWindowAdapter() {
+			mContent = getLayoutInflater().inflate(R.layout.info_content_layout, null);
+			mWindow = getLayoutInflater().inflate(R.layout.info_window_layout, null);
+		}
+		
+		@Override
+		public View getInfoContents(final Marker marker) {
+			if(marker.getTitle()!=null && marker.getSnippet() == null){
+				return mContent;
+			}
+			return null;
+		}
+
+		@Override
+		public View getInfoWindow(final Marker marker) {
+			if(marker.getTitle()==null && marker.getSnippet() == null){
+				return mWindow;
+			}
+			return null;
+		}
+
+		
+	}
+
 }
