88c0044536fee448294bfca4719bc202a42139e0
JanStalhut@gmail.com <JanStalhut@gmail.com@bc405e18-e45b-e554-df79-a0bdb10d3be3>
2013-02-01 23:05:45 +0000

    WebView with Google-Maps implemented; App-Icons replaced; shorter App-names

--- projects/2013_02_04_google_maps_api_battle/javascript/src/de/gdgbremen/mapsapibattle/javascript/MainActivity.java
+++ projects/2013_02_04_google_maps_api_battle/javascript/src/de/gdgbremen/mapsapibattle/javascript/MainActivity.java
@@ -5,69 +5,65 @@ import android.support.v4.app.FragmentTransaction;
 import de.gdgbremen.mapsapibattle.library.AbstractMainActivity;
 
 public class MainActivity extends AbstractMainActivity {
+	private MainFragment fragment;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		if (savedInstanceState == null) {
 			final FragmentTransaction transaction = getSupportFragmentManager()
 					.beginTransaction();
-			final MainFragment fragment = new MainFragment();
+			fragment = new MainFragment();
 			transaction.add(R.id.singlefragment, fragment);
 			transaction.commit();
+		} else {
+			fragment = (MainFragment) getSupportFragmentManager().findFragmentById(
+					R.id.singlefragment);
 		}
 	}
 
 	@Override
 	protected void showAbout() {
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	protected void showMarker() {
-		// TODO Auto-generated method stub
-		
+		fragment.showMarker();
 	}
 
 	@Override
 	protected void hideMarker() {
-		// TODO Auto-generated method stub
-		
+		fragment.hideMarker();
 	}
 
 	@Override
 	protected void showOverlays() {
-		// TODO Auto-generated method stub
-		
+		fragment.showOverlays();
 	}
 
 	@Override
 	protected void hideOverlays() {
-		// TODO Auto-generated method stub
-		
+		fragment.hideOverlays();
 	}
 
 	@Override
 	protected void showPosition() {
-		// TODO Auto-generated method stub
-		
+		fragment.showPosition();
 	}
 
 	@Override
 	protected void hidePosition() {
-		// TODO Auto-generated method stub
-		
+		fragment.hidePosition();
 	}
 
 	@Override
 	protected void showRoute() {
-		// TODO Auto-generated method stub
-		
+		fragment.showPosition();
 	}
 
 	@Override
 	protected void hideRoute() {
-		// TODO Auto-generated method stub
-		
+		fragment.hideRoute();
 	}
 }
--- projects/2013_02_04_google_maps_api_battle/javascript/src/de/gdgbremen/mapsapibattle/javascript/MainFragment.java
+++ projects/2013_02_04_google_maps_api_battle/javascript/src/de/gdgbremen/mapsapibattle/javascript/MainFragment.java
@@ -1,16 +1,74 @@
 package de.gdgbremen.mapsapibattle.javascript;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainFragment extends Fragment {
+
+	private WebView webview;
+
+	@SuppressLint("SetJavaScriptEnabled")
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
+		final View view = inflater
+				.inflate(R.layout.fragment_main, container, false);
+		webview = (WebView) view.findViewById(R.id.webview);
+		webview.getSettings().setJavaScriptEnabled(true);
+		webview.setWebViewClient(new WebViewClient() {
+			public void onReceivedError(WebView view, int errorCode,
+					String description, String failingUrl) {
+				Log.i("WebView", "(" + errorCode + ")" + description);
+			}
+		});
+		webview.loadUrl("file:///android_asset/map/googlemap.html");
+		return view;
+	}
 
-		return inflater.inflate(R.layout.fragment_main, container, false);
+	public void showMarker() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void hideMarker() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void showOverlays() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void hideOverlays() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void showPosition() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void hidePosition() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void showRoute() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void hideRoute() {
+		// TODO Auto-generated method stub
+
 	}
 }
