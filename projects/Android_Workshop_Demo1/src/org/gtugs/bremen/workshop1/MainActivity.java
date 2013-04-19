99017f266fb7f2fb6cc7ff4e6cbc7c1a4cea7cfe
Jan.Stalhut@gmail.com <Jan.Stalhut@gmail.com@bc405e18-e45b-e554-df79-a0bdb10d3be3>
2013-04-19 19:32:29 +0000

    add second activity and buttons for navigation

--- projects/Android_Workshop_Demo1/src/org/gtugs/bremen/workshop1/MainActivity.java
+++ projects/Android_Workshop_Demo1/src/org/gtugs/bremen/workshop1/MainActivity.java
@@ -1,8 +1,10 @@
 package org.gtugs.bremen.workshop1;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +19,12 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	/** Called when the user clicks the button */
+	public void openSecondActivity(View view) {
+		Intent intent = new Intent(this, SecondActivity.class);
+		this.startActivity(intent);
 	}
 
 }
